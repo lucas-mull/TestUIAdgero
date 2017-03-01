ecea11551074dd01df937f2bd70ef43804de53e6
Lucas Mull <lucas.mull@i-datech.fr>
2017-03-01 14:48:52 +0100

    Exemple simple de Gauge View pour représenter l'accélération

--- app/src/main/java/com/example/lucas/wireframesadgero/MainActivity.java
+++ app/src/main/java/com/example/lucas/wireframesadgero/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lucas.wireframesadgero;
 
+import android.os.AsyncTask;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,7 +20,12 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import pl.pawelkleczkowski.customgauge.CustomGauge;
+
 public class MainActivity extends AppCompatActivity {
+
+    final static int MIN_VALUE = 0;
+    final static int MAX_VALUE = 100;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -30,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    static boolean _changeValues = true;
+    static int _currentValue = MIN_VALUE;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -65,9 +74,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                _changeValues = !_changeValues;
             }
         });
+    }
 
+    private static void incrementValue()
+    {
+        if (_currentValue == MAX_VALUE)
+            _currentValue = MIN_VALUE;
+        else
+            _currentValue++;
     }
 
 
@@ -122,6 +139,34 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            final CustomGauge gaugeView =  (CustomGauge)rootView.findViewById(R.id.gaugeView);
+            final TextView value = (TextView) rootView.findViewById(R.id.txtValue);
+            new AsyncTask<Void, Void, Void>()
+            {
+
+                @Override
+                protected Void doInBackground(Void... params) {
+                    while (_changeValues)
+                    {
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        incrementValue();
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                gaugeView.setValue(_currentValue);
+                                value.setText("" + _currentValue);
+                            }
+                        });
+                    }
+
+                    return null;
+                }
+            }.execute();
             return rootView;
         }
     }
@@ -145,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
+            // Show 4 total pages.
             return 4;
         }
 
@@ -153,13 +198,13 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "BATTERIE";
+                    return "CHARGE";
                 case 1:
                     return "GPS";
                 case 2:
-                    return "STATISTIQUES";
+                    return "STATS";
                 case 3:
-                    return "ECOCONDUITE";
+                    return "ECO";
             }
             return null;
         }
